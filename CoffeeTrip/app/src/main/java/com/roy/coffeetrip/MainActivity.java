ce8188b371c79eee7ad8eed81d5882fcde0f8ac1
Roy-Luo <251909755@qq.com>
2016-05-10 15:30:03 +0800

    分支

--- CoffeeTrip/app/src/main/java/com/roy/coffeetrip/MainActivity.java
+++ CoffeeTrip/app/src/main/java/com/roy/coffeetrip/MainActivity.java
@@ -3,6 +3,9 @@ package com.roy.coffeetrip;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * CoffeeTrip
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
